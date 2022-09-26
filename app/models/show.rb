88f44df69975e1b09f57249a447e47685a666307
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-09-25 17:51:19 -0700

    commit tree 3c295a54088888562a7c684825f887c367b293e2

---
+++ app/models/show.rb
@@ -0,0 +1,25 @@
+class Show < ActiveRecord::Base
+    def self.highest_rating
+        self.maximum(:rating)
+    end
+    #How does below return the name? 
+    def self.most_popular_show
+        self.where("rating = ?", self.highest_rating).first
+    end
+    def self.lowest_rating
+        self.minimum(:rating)
+    end
+    #How does below return the name? 
+    def self.least_popular_show
+        self.where("rating = ?", self.lowest_rating).first
+    end
+    def self.ratings_sum
+        self.sum(:rating)
+    end
+    def self.popular_shows
+        self.where("rating > 5")
+    end
+    def self.shows_by_alphabetical_order
+        self.order(:name)
+    end
+end
